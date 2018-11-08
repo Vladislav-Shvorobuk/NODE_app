7ed1f60c2beb164bf9cbc41dc21e34b7c65b71e2
Vladislav <vladislav.shvorobuk@gmail.com>
2018-11-08 21:30:13 +0200

    add express, nodemon, npm start, npm dev

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const test = null;
+const express = require('express');
+const app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-
+app.listen(3000, () =>  console.log('Example app listening on port 3000!'));
