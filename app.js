5e880175275200195ee5c9e07f7ea3f4585ace39
Vladislav <vladislav.shvorobuk@gmail.com>
2018-11-18 15:52:56 +0200

    refactoring, customizing nodemon

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const path = require("path");
 const staticAsset = require("static-asset");
+const mongoose = require("mongoose");
+const config = require("./config");
 
+//database
 
+mongoose.Promise = global.Promise;
+mongoose.set("debug", config.IS_PRODUCTION);
+
+mongoose.connection
+    .on("error", error => console.log(error))
+    .on("close", () => console.log("Database connection has closed."))
+    .once("open", () => {
+      const info = mongoose.connections[0];
+      console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    });
+
+mongoose.connect(config.MONGO_URL, {useNewUrlParser: true });
+
+//express
 const app = express();
 
+//sets and uses
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(staticAsset(path.join(__dirname, "public")));
@@ -15,11 +33,30 @@ app.use(
   express.static(path.join(__dirname, "node_modules", "jquery", "dist"))
 );
 
-
+//routers
 app.get('/', (req, res) => {
  
      res.render("main");
-
 });
 
-module.exports = app;
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//err handler
+//eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.render("error", {
+    message: error.message,
+    error: !config.IS_PRODUCTION ? error : {}
+  });
+});
+
+
+app.listen(config.PORT, () => {
+  console.log(`Example app listening on port ${config.PORT}!`);
+});  
